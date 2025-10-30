55a0b8302aa93c9ca830e9eda0e0d50997b31b1c
Nhat Nguyen <nhatbg89@gmail.com>
2025-10-30 22:01:50 +0700

    init

---
+++ Sources/FeatureHome/FeatureHome.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import UIKit
+import CommonKit
+import UIComponents
+
+public final class HomeViewController: UIViewController {
+    public var user: UserModel?
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Home of \(user?.name ?? "Guest")"
+    }
+}
