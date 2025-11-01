c5a225863522fb4e22fa587f8dbb713dbdc5dd92
nhatduong <nhatbg89@gmail.com>
2025-11-01 23:36:54 +0700

    Update FeatureHome.swift

--- Sources/FeatureHome/FeatureHome.swift
+++ Sources/FeatureHome/FeatureHome.swift
@@ -11,7 +11,7 @@ public final class HomeViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
         title = "Home of \(user?.name ?? "Guest")"
     }
 }
